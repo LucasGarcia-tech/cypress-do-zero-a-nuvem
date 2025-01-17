b16028d393201001f50976f97b44cb9fbe011157
Lucas Garcia Da silva <144852975+LucasGarcia-tech@users.noreply.github.com>
2025-01-17 14:08:32 -0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -97,11 +97,32 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     
   })
 
-  it.only('seleciona um produto (YouTube) por seu texto', () => {
+  it('seleciona um produto (YouTube) por seu texto', () => {
   
-    cy.get('select').select('youtube')
+    cy.get('#product').select('YouTube')
       .should('have.value','youtube')
     
+  })
+
+  
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+  
+    cy.get('#product').select('mentoria')
+      .should('have.value','mentoria')
+    
+  })
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+  
+    cy.get('#product').select(1)
+      .should('have.value','blog')
+    
+  })
+
+  it.only('Marca o tipo de atendimento "Feedback"', () => {
+  
+    cy.get('input[type="radio"][value="Feedback"]').check()
+      .should('have.value','Feedback')
     
   })
 
